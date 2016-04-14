fdeb36e36239b30ecb3bb561767f519d8a65a9c4
ptdorf <ptdorf@gmail.com>
2016-04-14 03:46:48 +0200

    Engage open-source e-markets

--- can.gemspec
+++ can.gemspec
@@ -7,11 +7,11 @@ require "can"
 Gem::Specification.new do |spec|
   spec.name          = "can"
   spec.version       = Can::VERSION
-  spec.authors       = ["ptdorf"]
-  spec.email         = ["ptdorf@gmail.com"]
-  spec.homepage      = "https://rubygemspec.org/gems/can"
   spec.summary       = "Can stores encrypted goods."
   spec.description   = "Can stores encrypted goods"
+  spec.homepage      = "https://rubygemspec.org/gems/can"
+  spec.authors       = ["ptdorf"]
+  spec.email         = ["ptdorf@gmail.com"]
   spec.license       = "MIT"
 
   spec.files         = `git ls-files`.split $/
---
+++ lib/can/command.rb
@@ -0,0 +1,69 @@
+require "json"
+
+module Can
+  class Command
+
+    def initialize file, password
+      @password = password
+      @file = file
+    end
+
+    def content
+      read()
+      @content
+    end
+
+    def list
+      read()
+    end
+
+    def exists name
+      data = read()
+      data[name] ? true : false
+    end
+
+    def get name
+      data = read()
+      data[name] || nil
+    end
+
+    def copy value
+      IO.popen("pbcopy", "w") { |cc| cc.write(value) }
+      value
+    end
+
+    def set name, value
+      data = read()
+      data[name] = value
+      write(data)
+    end
+
+    def remove name
+      data = read()
+      data.delete name
+      write(data)
+    end
+
+    private
+    def read
+      return {} unless File.exist?(@file)
+
+      begin
+        content = Utils.read(@file, @password)
+      rescue Exception => e
+        p e
+        content = ""
+      end
+
+      abort "Fail to open file." unless content.length > 0
+      @content = content
+      data = JSON.parse(@content)
+    end
+
+    def write data
+      content = JSON.dump(data)
+      Utils.write(@file, @password, content)
+    end
+
+  end
+end
